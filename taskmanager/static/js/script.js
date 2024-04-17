5f5802749d278acf5a1e1cc59ae1d9059a192afc
Niklas Andersson <nianswe@hotmail.com>
2024-04-17 16:40:40 +0000

    Add Category form with Deletee

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
